1b1aa26beb7d1d0770b397344c3b6526f050b340
Miles Oldenburg <moldenbu@cisco.com>
2021-12-01 23:40:05 -0500

    Part 2

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -1,13 +1,30 @@
 class Day1 {
+    private val measurements = mutableListOf<Int>()
+
     init {
         val inputStream = javaClass.getResourceAsStream("/1/input.txt")
-        val measurements: MutableList<Int> = mutableListOf()
-        inputStream.bufferedReader().forEachLine { measurements.add(it.toInt()) }
+        inputStream.bufferedReader().forEachLine { this.measurements.add(it.toInt()) }
 
+        val part1Value = part1(this.measurements)
+        val part2Value = part2()
+
+        println("Part 1: $part1Value")
+        println("Part 2: $part2Value")
+    }
+
+    fun part1(measurements: MutableList<Int>): Int{
         var increasingMeasurements = 0
         for ((index, measurement) in measurements.withIndex()) {
             if (index > 0 && measurement > measurements[index - 1]) increasingMeasurements++
         }
-        println(increasingMeasurements)
+        return increasingMeasurements
+    }
+
+    fun part2(): Int {
+        val windows = mutableListOf<Int>()
+        for ((index, measurement) in this.measurements.withIndex()) {
+            if (index >= 2) windows.add(measurement + this.measurements[index - 1] + this.measurements[index - 2])
+        }
+        return this.part1(windows)
     }
 }
