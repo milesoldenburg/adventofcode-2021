054abe1d5a1f1ce487a03f7d815a7d1ff5df330b
Miles Oldenburg <moldenbu@cisco.com>
2021-12-05 21:22:12 -0500

    Day 5 Part 2

--- src/main/kotlin/days/Day5.kt
+++ src/main/kotlin/days/Day5.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 class Day5 {
     private val segments = mutableListOf<Array<IntArray>>()
 
@@ -32,9 +35,11 @@ class Day5 {
 
         val grid = Array(height + 1) { IntArray(width + 1) }
 
-        val part1Value = part1(grid)
+        //val part1Value = part1(grid)
+        val part2Value = part2(grid)
 
-        println("Part 1: $part1Value")
+        //println("Part 1: $part1Value")
+        println("Part 2: $part2Value")
     }
 
     private fun part1(grid: Array<IntArray>): Int {
@@ -44,36 +49,65 @@ class Day5 {
             val x2 = segment[1][0]
             val y2 = segment[1][1]
 
-            if (x1 == x2) {
-                if (y1 <= y2) {
-                    for (i in y1..y2) {
-                        grid[i][x1]++
-                    }
-                } else {
-                    for (i in y1 downTo y2) {
-                        grid[i][x1]++
-                    }
-                }
-            }
-
-            if (y1 == y2) {
-                if (x1 <= x2) {
-                    for (i in x1..x2) {
-                        grid[y1][i]++
-                    }
-                } else {
-                    for (i in x1 downTo x2) {
-                        grid[y1][i]++
+            if ((x1 == x2) || (y1 == y2)) {
+                for (x in min(x1, x2)..max(x1, x2)) {
+                    for (y in min(y1, y2)..max(y1, y2)) {
+                        grid[y][x]++
                     }
                 }
             }
         }
 
+        return computeDangerousAreas(grid)
+    }
+
+    private fun part2(grid: Array<IntArray>): Int {
+        for (segment in this.segments) {
+            val x1 = segment[0][0]
+            val y1 = segment[0][1]
+            val x2 = segment[1][0]
+            val y2 = segment[1][1]
+
+            if ((x1 == x2) || (y1 == y2)) {
+                for (x in min(x1, x2)..max(x1, x2)) {
+                    for (y in min(y1, y2)..max(y1, y2)) {
+                        grid[y][x]++
+                    }
+                }
+            } else if ((max(x1, x2) - min(x1, x2)) == (max(y1, y2) - min(y1, y2))) {
+                var xList: List<Int>
+                var yList: List<Int>
+
+                if (x1 <= x2) {
+                    xList = (x1..x2).toList()
+                } else {
+                    xList = (x1 downTo x2).toList()
+                }
+
+                if (y1 <= y2) {
+                    yList = (y1..y2).toList()
+                } else {
+                    yList = (y1 downTo y2).toList()
+                }
+
+                val xyList = xList.zip(yList)
+                for ((x, y) in xyList) {
+                    grid[y][x]++
+                }
+            }
+        }
+
+        return computeDangerousAreas(grid)
+    }
+
+    private fun computeDangerousAreas(grid: Array<IntArray>): Int {
         var dangerousAreas = 0
         for (row in grid) {
             for (column in row) {
+                print(column)
                 if (column >= 2) dangerousAreas++
             }
+            print('\n')
         }
 
         return dangerousAreas
